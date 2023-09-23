dcc742bea9d15ed37ec8429e85e7d7c9f539c8e2
estebanspitale <esteban_spitalehotmail.com>
2023-09-23 14:04:09 -0300

    L02: Design Principles Document

---
+++ lesson2/scripts/getdates.js
@@ -0,0 +1,5 @@
+const d = new Date();
+document.getElementById("currentyear").innerHTML = d.getFullYear();
+
+const a = new Date();
+document.getElementById("lastmodified").innerHTML = a.toLocaleString();
