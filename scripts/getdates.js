9a00d0ce2758f85d946f9b9aa10806b45a95f220
estebanspitale <esteban_spitalehotmail.com>
2023-09-28 17:40:06 -0300

    L03: Home Page Enhancement

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,13 @@ document.getElementById("currentyear").innerHTML = d.getFullYear();
 
 const a = new Date();
 document.getElementById("lastmodified").innerHTML = a.toLocaleString();
+
+/*Activity Lesson03 - Hamburger buttton*/
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
