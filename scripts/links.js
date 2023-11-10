a6165f4fc7091e9248c46904cf4ca252b664dc20
estebanspitale <esteban_spitalehotmail.com>
2023-11-09 23:46:07 -0300

    lessons

---
+++ scripts/links.js
@@ -0,0 +1,8 @@
+const baseURL = 'https://estebanspitale.github.io/wdd230/';
+const linksURL = 'https://estebanspitale.github.io/wdd230/data/links.json';
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+  }
